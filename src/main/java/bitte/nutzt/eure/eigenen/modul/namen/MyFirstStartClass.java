c833d5cc22288208535b1c727345b75b9e2df45a
Jonas Hecht <jonas.hecht@codecentric.de>
2020-11-29 21:57:18 +0100

    Add junit-jupiter dependency incl. surefire plugin for auto run in Maven builds. Also created initial Test case with assertion.

--- src/main/java/bitte/nutzt/eure/eigenen/modul/namen/MyFirstStartClass.java
+++ src/main/java/bitte/nutzt/eure/eigenen/modul/namen/MyFirstStartClass.java
@@ -2,8 +2,13 @@ package bitte.nutzt.eure.eigenen.modul.namen;
 
 class MyFirstStartClass {
 
+    private String name = "Rosalie";
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
 
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/bitte/nutzt/eure/eigenen/modul/namen/MyFirstStartClassTest.java
+++ src/test/java/bitte/nutzt/eure/eigenen/modul/namen/MyFirstStartClassTest.java
@@ -1,12 +1,20 @@
 package bitte.nutzt.eure.eigenen.modul.namen;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class MyFirstStartClassTest {
 
-    void check_if_something_does_something() {
+    @Test
+    void should_return_name_rosalie() {
         // Given
+        MyFirstStartClass myFirstStartClass = new MyFirstStartClass();
 
         // When
+        String name = myFirstStartClass.getName();
 
         // Then
+        assertEquals("Rosalie", name, "Her name should be Rosalie!");
     }
 }
